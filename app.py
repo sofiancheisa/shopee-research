043fb92aca4e949052a5f476b9b3b92f312511f3
sofiancheisa <sofian2207@gmail.com>
2024-11-03 01:32:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Accept": "application/json, text/plain, */*",
+    "Referer": "https://shopee.com.my/"
+}
 headers={
     "User-Agent": "Mozilla/5.0",
     "Accept-Language": "en-US,en;q=0.9",
